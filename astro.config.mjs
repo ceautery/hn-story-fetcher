1faf385c86c3ca1f0b8db9d2adcbe9c72e987809
ceautery <ceautery@gmail.com>
2025-06-18 12:27:28 -0400

    Node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import db from '@astrojs/db';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db()]
+  integrations: [db()],
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
