39781b573fa4f60a11ad78677fd40c38444f5dbc
ceautery <ceautery@gmail.com>
2025-06-19 08:22:13 -0400

    Cleanup
    * Move server rendering declaration to astro config
    * Remove unneeded TS type

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import db from '@astrojs/db';
+import db from "@astrojs/db";
 
-import node from '@astrojs/node';
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [db()],
-
+  output: "server",
   adapter: node({
-    mode: 'standalone'
-  })
+    mode: "standalone",
+  }),
 });
